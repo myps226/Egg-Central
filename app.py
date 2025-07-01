1a860f03bba9a67e4230d21ca00a062df129dd70
myps226 <mable8898@gmail.com>
2025-07-01 21:59:36 +0700

    Update app.py

--- app.py
+++ app.py
@@ -3,29 +3,24 @@ import requests
 
 app = Flask(__name__)
 
-
 @app.route("/")
 def index():
     return render_template("index.html")
-
 
 @app.route("/timer")
 def timer():
     return render_template("timer.html")
 
-
 @app.route("/recipes")
 def recipes():
     return render_template("recipes.html")
 
-
 @app.route("/get_egg_recipes")
 def get_egg_recipes():
     url = "https://www.themealdb.com/api/json/v1/1/filter.php?i=egg"
-    response = crequests.get(url)
+    response = requests.get(url)
     data = response.json()
     return jsonify(data)
-
 
 @app.route("/get_recipe_detail/<meal_id>")
 def get_recipe_detail(meal_id):
